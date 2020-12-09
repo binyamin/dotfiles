69809cd1910c344e7531fcf0e94108dbb3b2baa5
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-12-09 17:52:55 -0500

    Update install.sh with custom zsh theme

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ then
   cp -f ~/dotfiles/.zshrc ~/.zshrc
   chsh -s /usr/bin/zsh "$(whoami)"
   wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+  # Custom zsh theme
+  git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 fi
 
 sudo apt-get autoremove -y
