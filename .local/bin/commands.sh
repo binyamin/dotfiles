9bdfb02a30db34180c4f03b44adcf6f80aebb529
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-02-14 21:10:03 -0500

    add a command to snippets

--- .local/bin/commands.sh
+++ .local/bin/commands.sh
@@ -10,3 +10,7 @@ lsof -t -i:3000 | xargs kill
 
 # Minimize spotifym, when it starts in fullscreen and hides the taskbar
 wmctrl -r spotify -b toggle,fullscreen
+
+# List all manually-installed debian packages without dependencies
+# Source: https://unix.stackexchange.com/a/468369
+apt-mark showmanual | sort | grep -v -F -f <(apt show $(apt-mark showmanual) 2> /dev/null | grep -e ^Depends -e ^Pre-Depends | sed 's/^Depends: //; s/^Pre-Depends: //; s/(.*)//g; s/:any//g' | tr -d ',|' | tr ' ' '\n' | grep -v ^$ | sort -u)
