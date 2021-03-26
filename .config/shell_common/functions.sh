13584cfbe39f00a8bdb5f2cba8f7ac1dfce42e76
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-03-26 18:35:58 -0400

    another random update (03-26-2021)

--- .config/shell_common/functions.sh
+++ .config/shell_common/functions.sh
@@ -5,3 +5,9 @@ mksound() {
   # the `${}` syntax provides a default value for `$1`
   paplay "/usr/share/sounds/freedesktop/stereo/${1:-"message"}.oga"
 }
+
+timer() {
+  # $ timer <seconds> <title>
+  
+  sleep $1 && mksound & zenity --info --icon-name="time" --text="${2:-"Time's up!"}";
+}
