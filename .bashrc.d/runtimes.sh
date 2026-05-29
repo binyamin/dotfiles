9bbade4deb5cb3193934932d505ac7dc94ecbc1d
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-05-29 18:35:36 -0400

    Update runtimes.sh

--- .bashrc.d/runtimes.sh
+++ .bashrc.d/runtimes.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-
-## Init runtimes/languages ##
+###
+# Runtimes, languages & stuff
+###
 
 # Pyenv (python) setup
 if [ -d "$HOME/.pyenv/bin" ]; then
@@ -17,15 +17,10 @@ if [ -d "$HOME/.rbenv/bin" ]; then
   eval "$(rbenv init -)"
 fi
 
-# NVM (node) setup
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Fnm (node) setup
-if [ -d $HOME/.fnm ]; then
-  export PATH="$HOME/.fnm:$PATH"
-  eval "`fnm env`"
+if [ -d $HOME/.local/share/fnm ]; then
+  export PATH="$HOME/.local/share/fnm:$PATH"
+  eval `fnm env`
 fi
 
 # Go setup
@@ -33,22 +28,49 @@ if [ -d /usr/local/go ]; then
   export PATH="$PATH:/usr/local/go/bin"
 fi
 
+if [ -d $HOME/go ]; then
+  export PATH="$PATH:$HOME/go/bin"
+fi
+
 # Deno Setup
-export DENO_INSTALL="$HOME/.deno"
-if [ -d $DENO_INSTALL ]; then
+if [ -d "$HOME/.deno" ]; then
+  export DENO_INSTALL="$HOME/.deno"
   export PATH="$DENO_INSTALL/bin:$PATH"
 fi
 
-# Rust (cargo) setup
-export CARGO_DIR="$HOME/.cargo"
-if [ -d $CARGO_DIR ]; then
-  export PATH="$CARGO_DIR/bin:$PATH";
+# Bun Setup
+if [ -d "$HOME/.bun" ]; then
+  export BUN_INSTALL="$HOME/.bun"
+  export PATH="$BUN_INSTALL/bin:$PATH"
 fi
 
+# Rust (cargo) setup
+if [ -d "$HOME/.cargo" ]; then
+  export CARGO_DIR="$HOME/.cargo"
+  export PATH="$CARGO_DIR/bin:$PATH";
+  . "$CARGO_DIR/env"
+fi
 
-## other ##
+# Corepack (node.js) config
+export COREPACK_ENABLE_PROJECT_SPEC=0
+
+# pnpm global bin
+if [ -d "$HOME/.local/share/pnpm" ]; then
+  export PNPM_HOME="$HOME/.local/share/pnpm"
+  export PATH="$PNPM_HOME:$PATH"
+fi
 
 # yarn global installs
 if [ -d "$HOME/.yarn/bin" ]; then
   export PATH="$HOME/.yarn/bin:$PATH"
+fi
+
+# Turso
+if [ -d "$HOME/.turso" ]; then
+	export PATH="$HOME/.turso:$PATH"
+fi
+
+# Flutter
+if [ -d "$HOME/.flutter" ]; then
+	export PATH="$HOME/.flutter/bin:$PATH"
 fi
