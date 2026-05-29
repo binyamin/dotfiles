601d4fb2cd7877887ceba1a58d354f24b78eb885
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-05-29 17:55:40 -0400

    Update aliases.sh

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,37 +1,13 @@
-#!/bin/bash
-
 ###
-# Alias definitions
+# Aliases
 ###
 
-## Copied from .bashrc ##
-
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
+# Add an "alert" alias for long running commands.
+# Use like so: `sleep 10; alert`
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
-
-## General ##
-alias vsc="code-insiders"
-
-# Prefer pre-installed executable `time` over built-in shell keyword (h/t
-# https://askubuntu.com/a/86196)
+# Prefer pre-installed executable `time` over built-in shell
+# keyword (h/t https://askubuntu.com/a/86196)
 alias time='/usr/bin/time'
 
 # Prompt before removing a file or folder (h/t @thumb@fosstodon.org)
